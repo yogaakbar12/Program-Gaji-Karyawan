8688a895bf37ab94fa65535d7fa20d63ef3ff6d9
A063MuhammadIrfanAryaSyahputra <101864606+A063MuhammadIrfanAryaSyahputra@users.noreply.github.com>
2022-03-25 15:01:55 +0700

    Update Gaji Karyawan.cpp

--- Gaji Karyawan.cpp
+++ Gaji Karyawan.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 	int a,b;
-	float bonus1,team2;
+	long int bonus1,bonus2,c,d;
 	cout<<"Program menghiotung gaji\n";
 	cout<<"Gaji team 1 = 22 juta\n";
 	cout<<"Gaji team 2 = 28 juta\n";
 	cout<<"Bagi team yang dapat menyelesaikan dalam wakti kurang dari 5 bulan maka gaji ditambah 6%\n";
 	cout<<"=============================================================\n";
 	
-	cout<<"apakah team 1 dapat menyelesaikan kurang dalam 5 bulan?\n";
+	cout<<"apakah team 1 dapat menyelesaikan kurang dalam 5 bulan? (1/0) : ";
 	cin>>a;
+	cout<<"==================================================================\n";
 	if (a == 1){
-		bonus1=(6/100*22)+22;
+		bonus1=0.06*22000000;
+		c=22000000;
 		cout<<"Anda mendapatkan gaji bonus senilai "<<bonus1<<endl;
-		
-	}
-	else{
-		cout<<"Anda mendapatkan gaji normal\n";
-	}
-	
-	cout<<"Apakah team 2 dapat mengerjakan kurang dalam 5 bulan?\n";
-	cin>>b;
-	
-	if(b==1){
-		cout<<"anjayy\n";
-		// proses
-	}
-	
-	else{
-		cout<<"Anda mendapatkan gaji normal\n";
-	}
-	return 0;
-}
+		cout<<"Total Gaji Anda = " << bonus1+c<<endl;
+		cout<<"==================================================\n";
